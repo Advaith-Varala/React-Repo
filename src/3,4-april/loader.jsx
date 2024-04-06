d2bf5e25452911d96a07fa74199ba1b54f9faf8b
Advaith-Varala <varalaadvaith27@gmail.com>
2024-04-06 10:55:07 +0530

    04April/react/count#delete#update

---
+++ src/3,4-april/loader.jsx
@@ -0,0 +1,7 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+function BorderExample() {
+  return <Spinner style={{backgroundColor:"black"}}animation="border" />;
+}
+
+export default BorderExample;
